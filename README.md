c770811796470f424b66ab9688fd2fdf0ae25fee
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-06-13 14:03:01 -0400

    add ts-node-dev

---
+++ README.md
@@ -0,0 +1,8 @@
+Building an Express server with MongoDB and GraphQL
+
+Based on Egghead.io [course](https://egghead.io/lessons/graphql-create-express-server-in-node-js)
+
+```
+npm install
+npm start
+```
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
-import * as express from 'express';
+import express from 'express';
 
 const app = express()
 const PORT = 3000
+
+app.get('/', (req, res) => {
+  return res.json({
+    message: 'hello there'
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`server running at ${PORT}`)
 })
