9fee258075bcc07871be1b437e2748c530b19f35
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-06-21 16:17:13 -0400

    add apollo server

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { ApolloEngine } from 'apollo-engine'
 import { ApolloServer } from 'apollo-server'
 import express from 'express'
 import Knex from 'knex'
@@ -5,7 +6,11 @@ import morgan from 'morgan'
 import { Model } from 'objection'
 import schema from './schema'
 
-const server = new ApolloServer(schema)
+const server = new ApolloServer({
+  ...schema,
+  tracing: true,
+  cacheControl: true,
+})
 
 const knexConfig = require('../knexfile')
 
@@ -23,7 +28,6 @@ app.get('/', (req, res) => {
   })
 })
 
-// GraphQL playground at http://localhost:3000/graphql
 server.applyMiddleware({ app })
 
 // Error handling. The `ValidationError` instances thrown by objection.js have a `statusCode`
@@ -41,6 +45,25 @@ app.use((err: any, req: any, res: any, next: any) => {
   }
 })
 
-app.listen(PORT, () => {
+const onListen = () => {
   console.log(`server running at ${PORT}`)
-})
+  console.log(`GraphQL playground at ${PORT}/graphql`)
+}
+
+const apolloEngineApiKey = process.env.APOLLO_ENGINE_API_KEY
+if (apolloEngineApiKey) {
+  console.log('running Apollo Engine')
+  const engine = new ApolloEngine({
+    apiKey: apolloEngineApiKey,
+  })
+  engine.listen(
+    {
+      port: PORT,
+      expressApp: app,
+    },
+    onListen
+  )
+} else {
+  console.log('running Express server')
+  app.listen(PORT, onListen)
+}
